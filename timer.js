85f74f852ac3aa5136a31e07317f8df5f245666a
Bryan MacDiarmid <brymac@gmail.com>
2015-10-01 10:12:20 -0400

    Time updating with setInterval

--- timer.js
+++ timer.js
@@ -1,7 +1,28 @@
+var timer;
 var timerOn = false;
+var time = 0;
 $( document ).ready(function() {
   $( '#start_stop' ).click(function() {
     timerOn = !timerOn;
+    if (timerOn) {
+      $(this).text('Stop');
+      startTimer();
+    } else {
+      $(this).text('Start');
+      stopTimer();
+    }
   });
-}
-);
+
+  function startTimer() {
+    timer = setInterval(addTime, 1000)
+  }
+  function stopTimer() {
+    clearInterval(timer);
+  }
+  function addTime() {
+    time += 1000;
+    $('#timer-seconds').text(time)
+  }
+
+
+});
